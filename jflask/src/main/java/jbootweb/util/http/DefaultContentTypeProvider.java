9b9ee9eede9de180424b9e6bf87a73ab7104372f
pcdv <pcdv@server.fake>
2014-02-04 17:45:53 +0100

    support .woff mime type

--- jflask/src/main/java/jbootweb/util/http/DefaultContentTypeProvider.java
+++ jflask/src/main/java/jbootweb/util/http/DefaultContentTypeProvider.java
@@ -8,6 +8,8 @@ public class DefaultContentTypeProvider implements ContentTypeProvider {
       return "application/javascript";
     if (path.endsWith(".css"))
       return "text/css";
+    if (path.endsWith(".woff"))
+      return "application/x-font-woff";
 
     int dot = path.lastIndexOf(".");
     if (dot != -1)
